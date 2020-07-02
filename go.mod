6dd540e4f3a79f502752ed129d39b39735ed8ca1
Art <artshesha@protonmail.com>
2020-07-02 11:43:12 +0300

    Basic structure

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/artshesha/walletscan
 
 go 1.14
 
-require github.com/miguelmota/go-ethereum-hdwallet v0.0.0-20200123000308-a60dcd172b4c
+require (
+	github.com/ethereum/go-ethereum v1.9.5
+	github.com/miguelmota/go-ethereum-hdwallet v0.0.0-20200123000308-a60dcd172b4c
+	github.com/tyler-smith/go-bip39 v1.0.2
+)
--- walletscan.go
+++ walletscan.go
@@ -1,32 +1,83 @@
 package main
 
 import (
+    "os"
 	"fmt"
 	"log"
+    "bufio"
 
 	"github.com/miguelmota/go-ethereum-hdwallet"
+//    "github.com/tyler-smith/go-bip39/wordlists"
+    "github.com/ethereum/go-ethereum/accounts"
+    "github.com/ethereum/go-ethereum/common"
 )
 
-func main() {
-	mnemonic := "tag volcano eight thank tide danger coast health above argue embrace heavy"
+func guessFromFile(filename string) string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    scanner.Split(bufio.ScanLines)
+    scanner.Scan()
+
+    return scanner.Text()
+}
+
+func checkWallet(mnemonic string, account accounts.Account) bool {
 	wallet, err := hdwallet.NewFromMnemonic(mnemonic)
 	if err != nil {
 		log.Fatal(err)
 	}
+    deriveAccount(wallet)
 
+    return wallet.Contains(account)
+}
+
+func deriveAccount(wallet *hdwallet.Wallet) {
 	path := hdwallet.MustParseDerivationPath("m/44'/60'/0'/0/0")
-	account, err := wallet.Derive(path, false)
+	// path = hdwallet.MustParseDerivationPath("m/44'/60'/0'/0/1")
+	_, err := wallet.Derive(path, true)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(account.Address.Hex())
+	//fmt.Println(account.Address.Hex())
+}
 
-	// path = hdwallet.MustParseDerivationPath("m/44'/60'/0'/0/1")
-	// account, err = wallet.Derive(path, false)
-	// if err != nil {
-	//	log.Fatal(err)
-	// }
+func main() {
+    argv := os.Args
+    if len(argv) < 3 {
+        log.Fatal("Not enough arguments")
+    }
 
-	// fmt.Println(account.Address.Hex())
+    searchAddress := common.HexToAddress(argv[1])
+    guessFilename := argv[2]
+    account := accounts.Account{Address: searchAddress}
+    guessString := guessFromFile(guessFilename)
+
+    fmt.Printf("Looking who owns %s\n", account.Address.Hex())
+    fmt.Printf("Guess: %s\n", guessString)
+
+	mnemonic := guessString
+    fmt.Println(checkWallet(mnemonic, account))
+
+    //print(wordlists.English[2])
+}
+
+type Guess struct {
+    w01 []string
+    w02 []string
+    w03 []string
+    w04 []string
+    w05 []string
+    w06 []string
+    w07 []string
+    w08 []string
+    w09 []string
+    w10 []string
+    w11 []string
+    w12 []string
 }
